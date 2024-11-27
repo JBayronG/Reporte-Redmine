818095db0bb070af178cabcd91b48a804834fa37
JBayronG <bayrongrasiano12@gmail.com>
2024-11-27 17:00:57 -0500

    Implementación del comando para que se ejecute automaticamente en un horario especifico...

--- locators/RedmineBase.ts
+++ locators/RedmineBase.ts
@@ -20,23 +20,27 @@ export class RedmineBase {
     async expectVisible(selector: string){
         await expect(this.page.locator(selector)).toBeVisible({ timeout: 10000 });
     }
-    async ClickOn1(selector: string, index: number = 0) {
+    async clickOn1(selector: string, index: number = 0): Promise<void> {
         try {
             const locator = this.page.locator(selector);
             const count = await locator.count();
     
             if (count === 0) {
                 throw new Error(`No se encontró ningún elemento con el selector "${selector}".`);
-            } else if (count > 1) {
+            }
+    
+            if (count > 1) {
                 console.warn(
                     `Advertencia: Se encontraron ${count} elementos con el selector "${selector}". Usando el índice ${index}.`
                 );
             }
     
+            // Realiza el clic en el elemento del índice especificado
             await locator.nth(index).click();
         } catch (error) {
             console.error(`Error al hacer clic en el selector "${selector}":`, error);
             throw error;
         }
     }
+    
 }
